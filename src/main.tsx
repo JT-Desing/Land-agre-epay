8819a3bb4c889bf02fde0860c368308790dc5f43
JT-Desing <339070886+JT-Desing@users.noreply.github.com>
2026-05-22 23:24:45 -0500

    Initial landing setup from provided ePayco file

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import EpaycoAgregadorLanding from "./EpaycoAgregadorLanding";
+import "./styles.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <EpaycoAgregadorLanding />
+  </React.StrictMode>,
+);
